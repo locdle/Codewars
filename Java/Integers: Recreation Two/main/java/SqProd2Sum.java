c2dc0aaf3a28ec2be055d39d79f08c5f183993c1
locle <ducloc1803@gmail.com>
2016-03-27 11:36:10 -0700

    initial add integers: reaction 2

---
+++ Java/Integers: Recreation Two/main/java/SqProd2Sum.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * Created by locle on 3/27/16.
+ */
+public class SqProd2Sum {
+    public static List<long[]> prod2Sum(long a, long b, long c, long d) {
+        // your code
+        return null;
+
+    }
+}
