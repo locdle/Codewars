b367ad21c6a12bc9191c8a506c30c4613d4dcfd5
locle <ducloc1803@gmail.com>
2016-03-29 21:43:56 -0700

    add solutions

--- Java/IntegersRecreationTwo/main/java/SqProd2Sum.java
+++ Java/IntegersRecreationTwo/main/java/SqProd2Sum.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -5,8 +6,18 @@ import java.util.List;
  */
 public class SqProd2Sum {
     public static List<long[]> prod2Sum(long a, long b, long c, long d) {
-        // your code
-        return null;
+        int sum = (int) ((Math.pow(a, 2) + Math.pow(b,2)) *(Math.pow(c,2) + Math.pow(d,2)));
+        List<long[]> result = new ArrayList<long[]>();
+
+        int i = 1;
+        while(sum - Math.pow(i, 2) > 0){
+            if (Math.sqrt(sum - Math.pow(i, 2)) %1 == 0 ){
+                long[] temp  = new long[] {i, (long) Math.sqrt(sum- Math.pow(i, 2))};
+                result.add(temp);
+            }
+            i++;
+        }
+        return result;
 
     }
 }
